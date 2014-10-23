f0d2331c8139def9e5a37ba33bf866466d12ddde
Isaac Suttell <isaac@isaacsuttell.com>
2014-10-23 09:38:33 -0700

    Added silent failures
    Will wail silently now instead of crashing app

--- src/filters.php
+++ src/filters.php
@@ -10,134 +10,136 @@ use Isuttell\LaravelAnalytics\PageView as PageView;
 
 Route::filter('analytics', function()
 {
-	/*--------------------------------------------------------------------------
-	| Record Visitor Information
-	*/
+	try {
+		/*--------------------------------------------------------------------------
+		| Record Visitor Information
+		*/
 
-	$analyticsSession = null;
-
-
-	// Check to see if we already have data for this user
-	if(Session::has('visitoranalytics_id')) $analyticsSession = VisitorAnalytics::find(Session::get('visitoranalytics_id'));
+		$analyticsSession = null;
 
 
-	// If not create a new session
-	if(is_null($analyticsSession)) $analyticsSession = new VisitorAnalytics;
-
-	// Save the users IP and User Agent
-	$analyticsSession->ip         = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
-	$analyticsSession->user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
-	$analyticsSession->lang       = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '';
+		// Check to see if we already have data for this user
+		if(Session::has('visitoranalytics_id')) $analyticsSession = VisitorAnalytics::find(Session::get('visitoranalytics_id'));
 
 
-	// If the user is logged in store their id
-	if(Auth::check() && $analyticsSession->user_id === 0)
-	{
-		$analyticsSession->user_id = Auth::user()->id;
-	}
+		// If not create a new session
+		if(is_null($analyticsSession)) $analyticsSession = new VisitorAnalytics;
+
+		// Save the users IP and User Agent
+		$analyticsSession->ip         = isset($_SERVER['REMOTE_ADDR']) ? $_SERVER['REMOTE_ADDR'] : '';
+		$analyticsSession->user_agent = isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : '';
+		$analyticsSession->lang       = isset($_SERVER['HTTP_ACCEPT_LANGUAGE']) ? $_SERVER['HTTP_ACCEPT_LANGUAGE'] : '';
 
 
-	// Keep track of the last activity so we can see how long a user was on the site
-	$analyticsSession->last_activity = \Carbon\Carbon::now();
-
-
-	// If browscap is set up, save browser information
-	if(ini_get("browscap")) {
-		$browser = get_browser(null, true);
-		$analyticsSession->platform        = $browser['platform'];
-		$analyticsSession->browser         = $browser['browser'];
-		$analyticsSession->browser_version = $browser['version'];
-		$analyticsSession->crawler         = $browser['crawler'];
-		$analyticsSession->ismobiledevice  = $browser['ismobiledevice'];
-		$analyticsSession->cssversion      = $browser['cssversion'];
-	}
-
-	// If GeoIP module is loaded look up location
-	if(extension_loaded ('geoip'))
-	{
-		// Catch any errors when running geoip
-		try {
-			$geoIpRecord = geoip_record_by_name($_SERVER['REMOTE_ADDR']);
-	
-			//if we found a record parse it
-			if($geoIpRecord !== false)
-			{
-				$analyticsSession->location = '';
-				if(strlen( $geoIpRecord['city'] ) > 0 && strlen( $geoIpRecord['region'] ) > 0)
-				{
-					$analyticsSession->location = $geoIpRecord['city'] . ', ' . $geoIpRecord['region'];
-				}
-	
-	
-				//Create a GeoJSON compatibl object to store coords
-				$geoJson = new stdClass();
-				$geoJson->type = "point";
-				$geoJson->coordinates = array(
-						$geoIpRecord['latitude'],
-						$geoIpRecord['longitude']
-					);
-	
-				$analyticsSession->geo = json_encode($geoJson);
-			}
-		} catch (Exception $e) {
-		    Log::error($e);
-		}
-	}
-
-	// Save the data and store the id for access on the next page load
-	$analyticsSession->save();
-	Session::put('visitoranalytics_id', $analyticsSession->id);
-
-
-	/*
-	|--------------------------------------------------------------------------
-	| Record Page Views
-	|
-	*/
-
-	//For php versions less than 5.4
-	if (!function_exists('http_response_code')) {
-		function http_response_code()
+		// If the user is logged in store their id
+		if(Auth::check() && $analyticsSession->user_id === 0)
 		{
-			if(isset($_SERVER['REDIRECT_STATUS']))
-			{
-				return (int) $_SERVER['REDIRECT_STATUS'];
-			}
-			else
-			{
-				return false;
+			$analyticsSession->user_id = Auth::user()->id;
+		}
+
+
+		// Keep track of the last activity so we can see how long a user was on the site
+		$analyticsSession->last_activity = \Carbon\Carbon::now();
+
+
+		// If browscap is set up, save browser information
+		if(ini_get("browscap")) {
+			$browser = get_browser(null, true);
+			$analyticsSession->platform        = $browser['platform'];
+			$analyticsSession->browser         = $browser['browser'];
+			$analyticsSession->browser_version = $browser['version'];
+			$analyticsSession->crawler         = $browser['crawler'];
+			$analyticsSession->ismobiledevice  = $browser['ismobiledevice'];
+			$analyticsSession->cssversion      = $browser['cssversion'];
+		}
+
+		// If GeoIP module is loaded look up location
+		if(extension_loaded ('geoip'))
+		{
+			// Catch any errors when running geoip
+			try {
+				$geoIpRecord = geoip_record_by_name($_SERVER['REMOTE_ADDR']);
+
+				//if we found a record parse it
+				if($geoIpRecord !== false)
+				{
+					$analyticsSession->location = '';
+					if(strlen( $geoIpRecord['city'] ) > 0 && strlen( $geoIpRecord['region'] ) > 0)
+					{
+						$analyticsSession->location = $geoIpRecord['city'] . ', ' . $geoIpRecord['region'];
+					}
+
+
+					//Create a GeoJSON compatibl object to store coords
+					$geoJson = new stdClass();
+					$geoJson->type = "point";
+					$geoJson->coordinates = array(
+							$geoIpRecord['latitude'],
+							$geoIpRecord['longitude']
+						);
+
+					$analyticsSession->geo = json_encode($geoJson);
+				}
+			} catch (Exception $e) {
+			    Log::error($e);
 			}
 		}
-	}
 
-	// Generate a new page view
-	$pageView = new PageView;
+		// Save the data and store the id for access on the next page load
+		$analyticsSession->save();
+		Session::put('visitoranalytics_id', $analyticsSession->id);
 
-	// Save the visitoranalytics id so we can link the data
-	$pageView->visitoranalytics_id = $analyticsSession->id;
 
-	// HTTP Response Code: eg 200, 404, etc
-	$pageView->status              = http_response_code();
+		/*
+		|--------------------------------------------------------------------------
+		| Record Page Views
+		|
+		*/
 
-	// Save the URL of the page view
-	$pageView->url                 = Request::path() === '/' ? '/' : '/' . Request::path();
+		//For php versions less than 5.4
+		if (!function_exists('http_response_code')) {
+			function http_response_code()
+			{
+				if(isset($_SERVER['REDIRECT_STATUS']))
+				{
+					return (int) $_SERVER['REDIRECT_STATUS'];
+				}
+				else
+				{
+					return false;
+				}
+			}
+		}
 
-	// Check to see if the call is an ajax call
-	$pageView->ajax                = Request::ajax();
+		// Generate a new page view
+		$pageView = new PageView;
 
-	// Get the request method: GET, POST, PUT, DELETE
-	if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'])
-	{
-		$pageView->method          = strtoupper($_SERVER['REQUEST_METHOD']);
-	}
+		// Save the visitoranalytics id so we can link the data
+		$pageView->visitoranalytics_id = $analyticsSession->id;
 
-	// Save the referrer so we can track a users through the site
-	if(isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'])
-	{
-		$pageView->referrer        = $_SERVER['HTTP_REFERER'];
-	}
+		// HTTP Response Code: eg 200, 404, etc
+		$pageView->status              = http_response_code();
 
-	// And save
-	$pageView->save();
+		// Save the URL of the page view
+		$pageView->url                 = Request::path() === '/' ? '/' : '/' . Request::path();
+
+		// Check to see if the call is an ajax call
+		$pageView->ajax                = Request::ajax();
+
+		// Get the request method: GET, POST, PUT, DELETE
+		if(isset($_SERVER['REQUEST_METHOD']) && $_SERVER['REQUEST_METHOD'])
+		{
+			$pageView->method          = strtoupper($_SERVER['REQUEST_METHOD']);
+		}
+
+		// Save the referrer so we can track a users through the site
+		if(isset($_SERVER['HTTP_REFERER']) && $_SERVER['HTTP_REFERER'])
+		{
+			$pageView->referrer        = $_SERVER['HTTP_REFERER'];
+		}
+
+		// And save
+		$pageView->save();
+	} catch (Exception $e) { }
 
 });
